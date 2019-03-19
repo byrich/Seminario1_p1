e1c1f4af3e705a72877c199b6acb7db095677922
byrich <byrichcraft@gmal.com>
2019-03-18 22:41:11 -0600

    php otra vez

--- sample.js
+++ sample.js
@@ -1,9 +1,44 @@
 var AWS = require('aws-sdk');
-var uuid = require('uuid');
 
+var fs =  require('fs');
+
+var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Bucket names must be unique across all S3 users
+
+var myBucket = 'sem1-practica1';
+
+var myKey = 'jpeg';
+//for text file
+//fs.readFile('demo.txt', function (err, data) {
+//for Video file
+//fs.readFile('demo.avi', function (err, data) {
+//for image file        
+fs.readFile('demo.jpg', function (err, data) {
+  if (err) { throw err; }
+
+  
+
+     var params = {Bucket: myBucket, Key: myKey, Body: data };
+
+     s3.putObject(params, function(err, data) {
+
+         if (err) {
+
+             console.log(err)
+
+         } else {
+
+             console.log("Successfully uploaded data to myBucket/myKey");
+
+         }
+
+      });
+
+});
 // Create unique bucket name
 //var bucketName = 'node-sdk-sample-' + uuid.v4();
-var bucketName = 'sem1-practica1';
+/*var bucketName = 'sem1-practica1';
 // Create name for uploaded object key
 var keyName = 'hello_world.txt';
 
@@ -14,6 +49,7 @@ uploadPromise.then(
   function(data) {
     console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
   });
+
 
 // Create a promise on S3 service object
 //var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
