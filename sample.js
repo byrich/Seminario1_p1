22081817326c8bb35746e6916d0190421b8b1486
byrich <byrichcraft@gmal.com>
2019-03-18 22:18:13 -0600

    php otra vez

--- sample.js
+++ sample.js
@@ -2,7 +2,8 @@ var AWS = require('aws-sdk');
 var uuid = require('uuid');
 
 // Create unique bucket name
-var bucketName = 'node-sdk-sample-' + uuid.v4();
+//var bucketName = 'node-sdk-sample-' + uuid.v4();
+var bucketName = 'sem1-practica1';
 // Create name for uploaded object key
 var keyName = 'hello_world.txt';
 
