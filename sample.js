226d759812cf19538de5c160326681705ecc21da
byrich <byrichcraft@gmal.com>
2019-03-18 22:25:33 -0600

    php otra vez

--- sample.js
+++ sample.js
@@ -7,21 +7,23 @@ var bucketName = 'sem1-practica1';
 // Create name for uploaded object key
 var keyName = 'hello_world.txt';
 
+var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    // Create object upload promise
+var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+uploadPromise.then(
+  function(data) {
+    console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+  });
+
 // Create a promise on S3 service object
-var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+//var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
 
 // Handle promise fulfilled/rejected states
-bucketPromise.then(
+/*bucketPromise.then(
   function(data) {
     // Create params for putObject call
-    var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-    // Create object upload promise
-    var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
-    uploadPromise.then(
-      function(data) {
-        console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-      });
+    
 }).catch(
   function(err) {
     console.error(err, err.stack);
-});
+});*/
